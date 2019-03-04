0003bf217553a3298501ed1d45ee2fe9d845b394
Hugo Ribeiro <33881233+huribeir@users.noreply.github.com>
2019-03-04 16:42:22 +1100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,78 +3,28 @@
 
 
 #BeforeInstallWithTryCatch.PS1
-
-
-# Define global variables. $errvar will be used to store error messages upon command failures. This variable will be used to output the error message to a log file.
-
-
-$errvar
-
-
-# Here is where we set up the Try, Catch, and Finally blocks. The Try block is where we add the code that we want to execute.
-
-
-# The Catch block contains code that will execute if code in the Try block fails and generates an error.
-
-
-# The Finally block is used in this example to write a time stamp and the contents of $errvar to the specified log file.
-
-
+$errvar # Define global variable $errvar for storage of all error messages.
+# Create Try, Catch, and Finally blocks.
+# The Try block typically contains the deployment lifecycle event code
+# The Catch block runs if the Try block fails and sets the script exit code to 1.
+# The Finally block writes a time stamp and the contents of the global error variable to the specified log file.
 Try
-
-
 {
-
-
-        $ErrorActionPreference = ‘Stop’    # Change how PowerShell responds to non-terminating errors, this causes PowerShell to stop execution for all errors.
-
-
-        Get-InstalledModule -ev errvar    # The -ev parameter (-ErrorVariable) captures and writes errors to the global variable. It is set on each command.
-
-
-        Get-ACMCertificate -ev errvar    # This command will generate a missing parameter error
-
-
-        Get-Module -ev errvar
-
-
-        net user thiswillerror > c:\stderr.log 2>&1     # To handle the error outside of the try catch using a traditional DOS method
-
-
+  $ErrorActionPreference = 'Stop' # Causes PowerShell to stop execution for both terminating and non-terminating errors.
+  Get-InstalledModule -ev errvar # invoke -ev to write errors to the global error variable.
+  Get-ACMCertificate -ev errvar # Cause a missing parameter error and invoke -ev
+  Get-Module -ev errvar
+  net user thiswillerror > c:\stderr.log 2>&1 # Use legacy error logging and pass the ErrOut channel to stdout.
 }
-
-
-Catch
-
-
+Catch # The Catch block runs if an error occurs in the Try block.
 {
-
-
-    Write-Warning $_
-
-
-        if ($_ -ne "") {$errvar = $_}
-
-
-        exit 1&    #This command instructs the session to exit and return an exit code value of 1, which causes CodeDeploy to fail the deployment.
-
-
+  Write-Warning $_
+    if ($_ -ne "") {$errvar = $_} # If $_ is not an empty string, set the value of the global error variable to the contents of $_.
+    exit 1 # Exit the session and return an exit code of 1, which causes the CodeDeploy lifecycle event to fail
 }
-
-
 Finally
-
-
 {
-
-
-    $time=Get-Date    # Defines a time variable with the current date and time.
-
-
-    "Attempt to run the script was made at $time with error $errvar " | out-file C:\temp\scriptExec.log -Append    # Outputs string and error to designated file
-
-
+  $time=Get-Date # Create a timestamp variable and set value to the current date and time.
+  "Attempted to run script at $time with error $errvar " | out-file C:\temp\scriptExec.log -Append    
+  # Appends string and error to designated file.
 }
-
-
-# Append option will add to any existing content
